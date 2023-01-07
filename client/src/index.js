bb89319a41da1891db450d4a012218dedf42b938
daniel.costa <daniel.costa@tron.com.br>
2023-01-07 10:46:38 -0300

    Adicionando o material UI no projeto

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
